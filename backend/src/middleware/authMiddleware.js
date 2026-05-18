6add5f9f0fe0016ec178162becc18d41adc79183
DilipKumarKandula <kanduladilipkumar7@gmail.com>
2026-05-18 23:02:05 +0530

    Fix auth middleware cookie handling

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,38 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// const authMiddleware = (
-//   req,
-//   res,
-//   next
-// ) => {
-// const token = req.cookies.token;
-
-// if (!token) {
-//   return res.status(401).json({
-//     message: "No token provided",
-//   });
-// }
-
-//   try {
-//     const decoded = jwt.verify(
-//       token,
-//       process.env.JWT_SECRET
-//     );
-
-//     req.user = decoded;
-
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       message: "Invalid token",
-//     });
-//   }
-// };
-
-// module.exports = authMiddleware;
-
-
-
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (
@@ -40,18 +5,15 @@ const authMiddleware = (
   res,
   next
 ) => {
-  const authHeader =
-    req.headers.authorization;
+  const token =
+    req.cookies.token;
 
-  if (!authHeader) {
+  if (!token) {
     return res.status(401).json({
       message:
         "No token provided",
     });
   }
-
-  const token =
-    authHeader.split(" ")[1];
 
   try {
     const decoded =
