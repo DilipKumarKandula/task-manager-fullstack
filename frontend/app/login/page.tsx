1dcec808c34c7f307a6ba0b90726f1149f70301e
DilipKumarKandula <kanduladilipkumar7@gmail.com>
2026-05-18 19:15:51 +0530

    Test auto deployment

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,9 +7,9 @@ export default function LoginPage() {
   };
 
   return (
-    <main className="h-screen flex items-center justify-center">
+    <main className="h-screen flex items-center justify-center ">
       <div className="border p-10 rounded-xl">
-        <h1 className="text-2xl font-bold mb-6">
+        <h1 className="text-2xl font-bold mb-6 ">
           Login
         </h1>
 
